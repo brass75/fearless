7d964c2b5b5d40df4ca365e2798df8c9c01a4efa
Dan Shernicoff <dan@brassnet.biz>
2020-12-04 11:19:54 -0500

    Externalize namespace and key to config file

--- src/server.py
+++ src/server.py
@@ -10,6 +10,8 @@ HTML_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'html'
 STYLE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'styles'))
 CONFIG_FILE = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'config.json'))
 DEFAULT_PORT = 3000
+DEFAULT_KEY = "1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
+DEFAULT_NS = "dmstest"
 
 @app.route('/')
 def counter_page():
@@ -18,7 +20,7 @@ def counter_page():
 
 @app.route('/get_auth')
 def get_key():
-    return '{"key": "1ccb732e-b55a-4404-ad3f-0f99c02fe44e", "namespace": "dmstest"}'
+    return json.dumps({'port': port, 'key': key, 'namespace': namespace})
 
 @app.route('/styles/<file>')
 def get_style(file):
@@ -33,4 +35,6 @@ if __name__ == '__main__':
         with open(CONFIG_FILE) as f:
             config = json.load(f)
     port = config.get('port', DEFAULT_PORT)
+    key = config.get('key', DEFAULT_KEY)
+    namespace = config.get('namespace', DEFAULT_NS)
     app.run('0.0.0.0', port, debug=True)
